f4647872c9481a626d11364f201633b1624b9e2b
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-25 16:29:33 -0300

    Added dynamo resource

---
+++ dynamo.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "ddbtable" {
+  name           = "lambda"
+  hash_key       = "id"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
